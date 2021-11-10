6a406e73e9b2a6e97bdd25fe9c2b1e4638a98867
Darek <darekt@abmicro.pl>
2021-11-10 21:17:06 +0100

    Init test of items in Array

---
+++ Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    class Car
+    {
+
+        public string Brend { get; set; }
+        /// <summary>
+        /// Tworzenie obiektu samochód
+        /// </summary>
+        /// <param name="brend">Marka samochodu</param>
+        public Car(string brend)
+        {
+            Brend = brend;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.ArrayList list = new System.Collections.ArrayList();
+            list.Add(2);
+            list.Add("Adaś");
+            list.Add(new Car("BMW"));
+
+            Console.WriteLine(list.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
